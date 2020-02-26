276d3375ba148345b2582f37fd74a0bf097176fb
Gabriel.Correa <gabriel.correa@creditas.com.br>
2020-02-26 02:05:44 -0300

    Add commit method in transaction

--- src/main/kotlin/br/com/bank/account/Transaction.kt
+++ src/main/kotlin/br/com/bank/account/Transaction.kt
@@ -10,4 +10,10 @@ data class Transaction(val merchant: String, val amount: Long, val time: ZonedDa
             time = transactionRequest.time
         )
     }
+
+    fun commit(account: Account): Pair<Account?, List<Violation>> = when {
+        account == null -> Pair(first = null, second = listOf(Violation(key = "account-not-initialized", message = "Account not initialized")))
+        !account.activeCard -> Pair(first = account, second = listOf(Violation(key = "card-not-active", message = "Card not active"))
+        else -> account.commitTransaction(transaction = this)
+    }
 }
