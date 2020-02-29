56845d8a4125df1af8ebd73950b3ead0633d4ae8
Gabriel.Correa <gabriel.correa@creditas.com.br>
2020-02-29 01:30:50 -0300

    Add operation validation

---
+++ src/main/kotlin/violation/OperationValidation.kt
@@ -0,0 +1,11 @@
+package violation
+
+object OperationValidation {
+    //    val violations: List<OperationViolation>
+    fun findViolation(isViolated: () -> Boolean, violation: OperationViolation): OperationViolation? =
+            if (isViolated()) violation else null
+}
+
+fun OperationValidation.findViolation(isViolated: () -> Boolean, violation: OperationViolation): OperationViolation? =
+        if (isViolated()) violation else null
+
