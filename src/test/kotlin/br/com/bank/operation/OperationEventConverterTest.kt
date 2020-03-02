cd04d5fc6329794012a2412c3cb53ffab301d6dd
Gabriel.Correa <gabriel.correa@creditas.com.br>
2020-03-02 01:22:30 -0300

    Add unit tests for OperationEventConverter and OperationValidation

--- src/test/kotlin/br/com/bank/operation/OperationEventConverterTest.kt
+++ src/test/kotlin/br/com/bank/operation/OperationEventConverterTest.kt
@@ -1,5 +1,48 @@
 package br.com.bank.operation
 
+import br.com.bank.operation.account.Account
+import br.com.bank.operation.account.AccountOperationEvent
+import br.com.bank.operation.account.transaction.Transaction
+import br.com.bank.operation.account.transaction.TransactionOperationEvent
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import java.time.ZonedDateTime
 
-class OperationEventConverterTest
+class OperationEventConverterTest {
+    private lateinit var operationEventConverter: OperationEventConverter
+
+    @Before
+    fun init() {
+        val mapper = ObjectMapper().registerModule(KotlinModule())
+                .registerModule(JavaTimeModule())
+                .enable(DeserializationFeature.UNWRAP_ROOT_VALUE)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+
+        operationEventConverter = OperationEventConverter(mapper)
+    }
+
+    @Test
+    fun `should convert account message to AccountOperationEvent`() {
+        val accountJson = "{\"account\": {\"active-card\": true, \"available-limit\": 100} }"
+        val operationEvent = operationEventConverter.convertEvents(listOf(accountJson)).first() as AccountOperationEvent
+
+        assertEquals(true, operationEvent.activeCard)
+        assertEquals(100, operationEvent.availableLimit)
+    }
+
+    @Test
+    fun `should convert account message to TransactionOperationEvent`() {
+        val transactionJson = "{\"transaction\": {\"merchant\": \"Burguer King\", \"amount\": 20, \"time\": \"2019-02-13T11:00:00.000Z\" }}"
+        val operationEvent = operationEventConverter.convertEvents(listOf(transactionJson)).first() as TransactionOperationEvent
+
+        assertEquals("Burguer King", operationEvent.merchant)
+        assertEquals(20, operationEvent.amount)
+        assertEquals(ZonedDateTime.parse("2019-02-13T11:00:00.000Z[UTC]"), operationEvent.time)
+    }
+}
--- src/test/kotlin/br/com/bank/operation/validation/OperationValidationTest.kt
+++ src/test/kotlin/br/com/bank/operation/validation/OperationValidationTest.kt
@@ -1,5 +1,26 @@
 package br.com.bank.operation.validation
 
-import org.junit.Assert.*
+import br.com.bank.operation.validation.violation.InsufficientLimitViolation
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
 
-class OperationValidationTest
+class OperationValidationTest {
+    @Test
+    fun `should return null when not true the function that valid the violation`() {
+        val availableLimit = 1
+        val transactionAmount = 1
+
+        assertNull(OperationValidation.hasViolation({ availableLimit < transactionAmount }, InsufficientLimitViolation))
+    }
+
+    @Test
+    fun `should return the violations when true the function that valid the violation`() {
+        val availableLimit = 1
+        val transactionAmount = 2
+
+        assertEquals(InsufficientLimitViolation, OperationValidation
+                .hasViolation({ availableLimit < transactionAmount }, InsufficientLimitViolation)
+        )
+    }
+}
