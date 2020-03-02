21124669bbae37f468e8874e00113bddbaac5912
Gabriel.Correa <gabriel.correa@creditas.com.br>
2020-03-01 21:27:25 -0300

    Add account operation processor

--- src/test/kotlin/br/com/bank/operation/account/AccountOperationProcessorTest.kt
+++ src/test/kotlin/br/com/bank/operation/account/AccountOperationProcessorTest.kt
@@ -1,5 +1,39 @@
 package br.com.bank.operation.account
 
-import org.junit.Assert.*
+import br.com.bank.operation.validation.violation.AccountAlreadyInitializedViolation
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class AccountOperationProcessorTest
+class AccountOperationProcessorTest {
+    private lateinit var subject: AccountOperationProcessor
+
+    @Before
+    fun init() {
+        subject = AccountOperationProcessor()
+    }
+
+    @Test
+    fun `should create account when process the operation`() {
+        val account = Account(activeCard = true, availableLimit = 100)
+        val operationResult = subject.process(account)
+
+        assertEquals(account.activeCard, operationResult.account?.activeCard)
+        assertEquals(account.availableLimit, operationResult.account?.availableLimit)
+        assertEquals(account.transactions, operationResult.account?.transactions)
+        assertTrue(operationResult.violations.isEmpty())
+    }
+
+    @Test
+    fun `should not create account when process account already exists`() {
+        val account = Account(activeCard = true, availableLimit = 100)
+        AccountRepository.createAccount(account)
+
+        val operationResult = subject.process(account)
+
+
+        assertTrue(operationResult.violations.isNotEmpty())
+        assertEquals(AccountAlreadyInitializedViolation().reason, operationResult.violations.first().reason)
+    }
+}
