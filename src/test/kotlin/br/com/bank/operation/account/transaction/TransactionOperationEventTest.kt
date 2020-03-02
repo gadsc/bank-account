15801f62dc7b78d4a57f54bf13013041f207f8ea
Gabriel.Correa <gabriel.correa@creditas.com.br>
2020-03-01 21:52:14 -0300

    Add transaction operation event unit test

--- src/test/kotlin/br/com/bank/operation/account/transaction/TransactionOperationEventTest.kt
+++ src/test/kotlin/br/com/bank/operation/account/transaction/TransactionOperationEventTest.kt
@@ -1,5 +1,19 @@
 package br.com.bank.operation.account.transaction
 
-import org.junit.Assert.*
+import br.com.bank.operation.OperationIdentifier
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.time.ZonedDateTime
 
-class TransactionOperationEventTest
+class TransactionOperationEventTest {
+    @Test
+    fun `should convert operation event to operation`() {
+        val subject = TransactionOperationEvent(merchant = "Burguer King", amount = 100, time = ZonedDateTime.now())
+        val transactionOperation = subject.toOperation() as Transaction
+
+        assertEquals(subject.merchant, transactionOperation.merchant)
+        assertEquals(subject.amount, transactionOperation.amount)
+        assertEquals(subject.time, transactionOperation.time)
+        assertEquals(OperationIdentifier.TRANSACTION, transactionOperation.getIdentifier())
+    }
+}
