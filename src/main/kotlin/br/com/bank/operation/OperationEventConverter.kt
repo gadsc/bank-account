8fe1c1e55e791c951c254a774fa214032877edf2
Gabriel.Correa <gabriel.correa@creditas.com.br>
2020-03-02 01:41:55 -0300

    Add operation consumer test

--- src/main/kotlin/br/com/bank/operation/OperationEventConverter.kt
+++ src/main/kotlin/br/com/bank/operation/OperationEventConverter.kt
@@ -7,10 +7,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 class OperationEventConverter(
         private val mapper: ObjectMapper
 ) {
+    companion object {
+        private const val JSON_ROOT_POSITION = 2
+    }
+
     fun convertEvents(events: List<String>): List<OperationEvent> = events.map { convertEvent(it) }
 
     private fun convertEvent(event: String): OperationEvent = when {
-        event.contains(OperationIdentifier.ACCOUNT.identifier) ->
+        event.startsWith(prefix = OperationIdentifier.ACCOUNT.identifier, startIndex = JSON_ROOT_POSITION) ->
             mapper.readValue(event, AccountOperationEvent::class.java) as OperationEvent
         else ->
             mapper.readValue(event, TransactionOperationEvent::class.java) as OperationEvent
--- src/test/kotlin/br/com/bank/operation/consumer/OperationConsumerTest.kt
+++ src/test/kotlin/br/com/bank/operation/consumer/OperationConsumerTest.kt
@@ -1,5 +1,53 @@
 package br.com.bank.operation.consumer
 
+import br.com.bank.infra.Reader
+import br.com.bank.operation.OperationEventConverter
+import br.com.bank.operation.account.AccountOperationEvent
+import br.com.bank.operation.account.transaction.TransactionOperationEvent
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import java.time.ZonedDateTime
 
-class OperationConsumerTest
+class OperationConsumerTest {
+    private lateinit var subject: OperationConsumer
+
+    @Before
+    fun init() {
+        val accountJson = "{\"account\": {\"active-card\": true, \"available-limit\": 100} }"
+        val transactionJson = "{\"transaction\": {\"merchant\": \"Burguer King\", \"amount\": 20, \"time\": \"2019-02-13T11:00:00.000Z\" }}"
+        val events = listOf(accountJson, transactionJson)
+        val reader: Reader = mockk(relaxed = true)
+
+        every { reader.recursiveRead(any()) } returns events
+
+        val mapper = ObjectMapper().registerModule(KotlinModule())
+                .registerModule(JavaTimeModule())
+                .enable(DeserializationFeature.UNWRAP_ROOT_VALUE)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+
+        val operationEventConverter = OperationEventConverter(mapper)
+        subject = OperationConsumer(operationEventConverter = operationEventConverter, reader = reader)
+    }
+
+
+    @Test
+    fun `should consume events`() {
+        val operationEvents = subject.consume()
+
+        assertEquals(2, operationEvents.size)
+        assertTrue(
+                operationEvents.containsAll(listOf(
+                        AccountOperationEvent(true, 100),
+                        TransactionOperationEvent("Burguer King", 20, ZonedDateTime.parse("2019-02-13T11:00:00.000Z[UTC]"))
+                ))
+        )
+    }
+}
