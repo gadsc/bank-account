4add54c5ea6323c568f83207c1789fe65d18478f
Gabriel.Correa <gabriel.correa@creditas.com.br>
2020-03-01 21:20:30 -0300

    Add account operation event tests

--- src/test/kotlin/br/com/bank/operation/account/AccountOperationEventTest.kt
+++ src/test/kotlin/br/com/bank/operation/account/AccountOperationEventTest.kt
@@ -1,5 +1,17 @@
 package br.com.bank.operation.account
 
+import br.com.bank.operation.OperationIdentifier
 import org.junit.Assert.*
+import org.junit.Test
 
-class AccountOperationEventTest
+class AccountOperationEventTest {
+    @Test
+    fun `should convert operation event to operation`() {
+        val subject = AccountOperationEvent(activeCard = true, availableLimit = 100)
+        val accountOperation = subject.toOperation() as Account
+
+        assertEquals(subject.activeCard, accountOperation.activeCard)
+        assertEquals(subject.availableLimit, accountOperation.availableLimit)
+        assertEquals(OperationIdentifier.ACCOUNT, accountOperation.getIdentifier())
+    }
+}
